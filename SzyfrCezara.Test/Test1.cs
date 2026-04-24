12026dd34118f7eea7c9202bd83646673bf6a294
provilska <sofiya.p07@gmail.com>
2026-04-24 09:34:17 +0200

    Add files via upload

---
+++ SzyfrCezara.Test/Test1.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.ConstrainedExecution;
+using SzyfrCezara; 
+
+namespace SzyfrCezara.Testy
+{
+    [TestClass]
+    public class CezarTests
+    {
+        [TestMethod]
+        public void Test_KluczDodatni()
+        {
+            var szyfr = new Cezar("abc", 3);
+
+            string wynik = szyfr.Szyfruj();
+
+            Assert.AreEqual("def", wynik);
+        }
+
+        [TestMethod]
+        public void Test_KluczUjemny()
+        {
+            var szyfr = new Cezar("abc", -3);
+            string wynik = szyfr.Szyfruj();
+            Assert.AreEqual("xyz", wynik);
+        }
+
+        [TestMethod]
+        public void Test_Spacja()
+        {
+            var szyfr = new Cezar("ala ma", 1);
+            string wynik = szyfr.Szyfruj();
+            Assert.AreEqual("bmb nb", wynik);
+        }
+
+        [TestMethod]
+        public void Test_DuzyKlucz()
+        {
+            var szyfr = new Cezar("a", 28);
+            string wynik = szyfr.Szyfruj();
+            Assert.AreEqual("c", wynik);
+        }
+    }
+}
---
+++ SzyfrCezara/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SzyfrCezara
+{
+    public class Cezar
+    {
+        private string tekst;
+        private int klucz;
+
+        public Cezar(string tekst, int klucz)
+        {
+            this.tekst = tekst;
+            this.klucz = klucz;
+        }
+
+        public string Szyfruj()
+        {
+            string wynik = "";
+
+            int znormalizowanyKlucz = ((klucz % 26) + 26) % 26;
+
+            foreach (char znak in tekst)
+            {
+                if (znak == ' ')
+                {
+                    wynik += znak;
+                }
+                else
+                {
+                    int kodAscii = (int)znak;
+                    int nowaPozycja = (kodAscii - 97 + znormalizowanyKlucz) % 26 + 97;
+                    wynik += (char)nowaPozycja;
+                }
+            }
+            return wynik;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj tekst (małe litery): ");
+            string wczytanyTekst = Console.ReadLine();
+
+            Console.Write("Podaj klucz (liczba całkowita): ");
+            if (int.TryParse(Console.ReadLine(), out int wczytanyKlucz))
+            {
+                Cezar szyfrator = new Cezar(wczytanyTekst, wczytanyKlucz);
+                string wynik = szyfrator.Szyfruj();
+
+                Console.WriteLine("---");
+                Console.WriteLine($"Tekst zaszyfrowany: {wynik}");
+            }
+            else
+            {
+                Console.WriteLine("Błędny klucz. Podaj liczbę całkowitą.");
+            }
+        }
+    }
+}
